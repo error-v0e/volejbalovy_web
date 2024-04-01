b5dd86fc10281b5c0083118f14a821fa0708b878
error-v0e <Pej2202@seznam.cz>
2024-04-01 21:30:36 +0200

    Refactor sponzor popup functionality

--- controllers.js
+++ controllers.js
@@ -148,29 +148,7 @@ router.post("/sprava_klubu", upload.fields([{ name: 'logo', maxCount: 1 }, { nam
   return res.redirect("/sprava");
 });
 router.post("/sponzori_popup", async (req, res) => {
-  const { id } = req.body;
-  console.log("-----------------------------------------------------");
-  console.log(id);
-  console.log("-----------------------------------------------------");
-  if (id) {
-    if (req.session.name) { 
-      var name = req.session.name; 
-      try {
-          const kluby = await Klub.findOne();
-          const kategorie = await Kategorie.findAll({
-            attributes: ['id_kategorie','nazev', 'href'],
-            order: [['poradi', 'ASC']]
-          });
-          const sponzori = await Sponzor.findAll();
-          res.render('admin_views/sprava', {res, kluby, kategorie, sponzori});
-      } catch (error) {
-          console.error(error);
-          res.status(500).send('Chyba serveru');
-      }
-    } else {
-      return res.redirect("login");
-    }
-  }
+  
 });
 
 module.exports = router;
